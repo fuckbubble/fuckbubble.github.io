9cdb80d889fb887e2de55d43c6b3bdc998791b13
fuckbubble <154749854+fuckbubble@users.noreply.github.com>
2025-05-17 23:18:10 +0530

    Update cock.js

--- cock.js
+++ cock.js
@@ -16,16 +16,24 @@ function updateClock() {
 
 updateClock();
 
-// Dynamically add or change favicon
-function setFavicon(url) {
-  let link = document.querySelector("link[rel~='icon']");
-  if (!link) {
-    link = document.createElement('link');
-    link.rel = 'icon';
-    document.getElementsByTagName('head')[0].appendChild(link);
-  }
-  link.href = url;
+function addFavicons() {
+  const head = document.head;
+
+  const links = [
+    { rel: "icon", type: "image/png", href: "/favicon-96x96.png", sizes: "96x96" },
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "shortcut icon", href: "/favicon.ico" },
+    { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+    { rel: "manifest", href: "/site.webmanifest" }
+  ];
+
+  links.forEach(attrs => {
+    const link = document.createElement("link");
+    for (const key in attrs) {
+      link.setAttribute(key, attrs[key]);
+    }
+    head.appendChild(link);
+  });
 }
 
-// Set your favicon (replace with your favicon path)
-setFavicon('favicon.ico');
+addFavicons();

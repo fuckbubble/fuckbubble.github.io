6ea47ded0196f4cb146f5f427899b94a5e4a3b0b
fuckbubble <154749854+fuckbubble@users.noreply.github.com>
2025-05-17 23:13:47 +0530

    Update cock.js

--- cock.js
+++ cock.js
@@ -1,17 +1,31 @@
 function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
 
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+  hours = hours < 10 ? '0' + hours : hours;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    var timeString = hours + ':' + minutes + ':' + seconds;
-    document.getElementById('clock').innerText = timeString;
+  var timeString = hours + ':' + minutes + ':' + seconds;
+  document.getElementById('clock').innerText = timeString;
 
-    setTimeout(updateClock, 1000); 
+  setTimeout(updateClock, 1000);
+}
+
+updateClock();
+
+// Dynamically add or change favicon
+function setFavicon(url) {
+  let link = document.querySelector("link[rel~='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
   }
+  link.href = url;
+}
 
-  updateClock();
+// Set your favicon (replace with your favicon path)
+setFavicon('favicon.ico');
